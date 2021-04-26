2b82d1f37c208ef236dec714f00d57d3c4b51b28
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-26 10:05:14 +0700

    Create BPSA.cpp

---
+++ Câu lệnh rẽ nhánh/BPSA.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long a;
+	cin >> a;
+	if (a > 0) cout << a*a*a << ' '; else cout << a*a << ' ';
+	cin >> a;
+	if (a > 0) cout << a*a*a << ' '; else cout << a*a << ' ';
+	cin >> a;
+	if (a > 0) cout << a*a*a << ' '; else cout << a*a << ' ';
+}
