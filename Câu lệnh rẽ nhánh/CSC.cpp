3f95ac7c4d1e70ba3a12b0012809fc034b7c146f
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-26 10:05:37 +0700

    Create CSC.cpp

---
+++ Câu lệnh rẽ nhánh/CSC.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	long long a, b, c;
+	cin >> a >> b >> c;
+	if (a + c == b*2) cout << a + b + c;
+	else cout << "KHONG PHAI CAP SO CONG";
+}
