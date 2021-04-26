e4d09a6b3aaf72f86fc8f4758b773a2206e4a2e2
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-27 06:59:48 +0700

    Create MANG64.cpp

---
+++ Mảng hai chiều/MANG64.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define FOR(i, l, r) for(int i = l; i <= r; i++)
+#define REP(i, l, r) for(int i = l; i < r; i++)
+using namespace std;
+int n, m, a[100005], res, k, x;
+int main()
+{
+	cin >> n;
+	FOR(i,1,n) { FOR(j,1,n) cout << i+j << ' '; cout << '\n'; }
+}
+    
