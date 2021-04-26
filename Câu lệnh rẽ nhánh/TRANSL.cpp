0d94e568fef5a1179e8bf60c3873cfb2ff76d9df
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-26 10:06:42 +0700

    Create TRANSL.cpp

---
+++ Câu lệnh rẽ nhánh/TRANSL.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string s[9] = {"","","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+int main()
+{
+	int x;
+	cin >> x;
+	cout << s[x];
+}
