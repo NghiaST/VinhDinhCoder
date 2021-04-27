8056320a82efd4f54a88cbf5dcef8ee7327a03d4
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-27 07:03:13 +0700

    Create SDAONGUOC.cpp

---
+++ Kiểu xâu/SDAONGUOC.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define REPD(i, l, r) for(int i = r-1; i >=l; i--)
+using namespace std;
+char s[100005];
+int main()
+{
+	gets(s);
+	REPD(i,0,strlen(s)) putchar(s[i]);
+}
+    
