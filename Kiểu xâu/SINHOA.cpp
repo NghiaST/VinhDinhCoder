45e8a52269d3134c26eba0f3610c5f8bee1de76d
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-27 07:02:42 +0700

    Create SINHOA.cpp

---
+++ Kiểu xâu/SINHOA.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#define FOR(i, l, r) for(int i = l; i <= r; i++)
+#define REP(i, l, r) for(int i = l; i < r; i++)
+using namespace std;
+int n, m;
+long long a[100005], res, k, x;
+char s[100005];
+int main()
+{
+	gets(s);
+	REP(i,0,strlen(s)) putchar(toupper(s[i]));
+}
+    
