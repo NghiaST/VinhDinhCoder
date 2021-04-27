c40b6b056e74580e377006644cfbc031cda164c7
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-27 19:49:17 +0700

    Create S5CUOI.cpp

---
+++ Kiểu xâu/S5CUOI.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define FOR(i, l, r) for(int i = l; i <= r; i++)
+#define REP(i, l, r) for(int i = l; i < r; i++)
+#define REPD(i, l, r) for(int i = r-1; i >=l; i--)
+using namespace std;
+int n, m;
+long long a[100005], res, k, x;
+char s[100005];
+int main()
+{
+	gets(s); res = strlen(s);
+	REP(i,strlen(s) - 5,strlen(s)) cout << s[i];
+}
+    
