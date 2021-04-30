3f54742abfe3ce23819ee4120a9af00be2e8d7fb
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-30 17:05:25 +0700

    Create LOP.cpp

---
+++ FILE (không bao gồm bài KILUCDIEM/LOP.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define REP(i, l, r) for(int i = l; i < r; i++)
+using namespace std;
+long long n;
+char s[100005];
+int main()
+{
+    while (gets(s)) {
+        n = strlen(s);
+        while (n > 0 && s[n-1] != ' ') n--;
+        REP(i,n,strlen(s)) putchar(s[i]);
+        putchar('\n');
+    }
+}
