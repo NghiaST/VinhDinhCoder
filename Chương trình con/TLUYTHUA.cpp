13a8907bcc5b758f2583f4cd351be5b6096944cd
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-30 17:09:45 +0700

    Create TLUYTHUA.cpp

---
+++ Chương trình con/TLUYTHUA.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long a,b,c,d,e,f,g,h;
+int main()
+{
+    cin >> a >> b >> c >> d >> e >> f >> g >> h;
+    cout << (long long) (pow(a, e) + pow(b,f) + pow(c,g) + pow(d, h));
+}
+    
