e8a6bae26fc676b5ae4f4fa62c4010c39b43afa2
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-30 16:59:33 +0700

    Create UOC.pas

---
+++ FILE/UOC.pas
@@ -0,0 +1,25 @@
+const
+        fi='INPUT.TXT';
+        fo='OUTPUT.TXT';
+var
+        n,i,j:longint;
+        a:array[1..1000000]of longint;
+
+begin
+        {$ifndef ONLINE_JUDGE}
+        assign(input,fi);reset(input);
+        assign(output,fo);rewrite(output);
+        {$endif}
+        readln(n);
+        j:=0;
+        for i:=1 to trunc(sqrt(n)) do
+        if n  mod i=0 then
+        begin
+                inc(j);a[j]:=i;
+        end;
+        writeln('Cac uoc so cua ',n);
+        for i:=1 to j-1 do writeln(a[i]);
+        if a[j]*a[j]<>n then writeln(a[j]);
+        for i:=j downto 1 do writeln(n div a[i]);
+end.
+    
