e5711a9e73ae03ec1b435eff11504073788f123b
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-04-30 21:33:47 +0700

    Create PINOCCHIO.cpp

---
+++ Ước số - Bội số/PINOCCHIO.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long n, x, y;
+int main() {
+    scanf("%I64d%I64d", &n, &x);
+    while (--n) {
+        scanf("%I64d", &y);
+        x = __gcd(x, y);
+    }
+    cout << x;
+}
+    
