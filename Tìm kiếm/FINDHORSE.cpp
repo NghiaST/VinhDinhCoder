6c66471d3e7cddf8f84c9f74cf3e11b008c18dbb
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-05-04 22:41:44 +0700

    Create FINDHORSE.cpp

---
+++ Tìm kiếm/FINDHORSE.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#define FOR(i, l, r) for(int i=l; i<=r; i++)
+#define N 100005
+using namespace std;
+long long n, a[N], b[N];
+
+int main() {
+    scanf("%lld", &n);
+    FOR(i, 1, n) scanf("%lld", &a[i]);
+    FOR(i, 1, n) scanf("%lld", &b[i]);
+    sort(b + 1, b + n + 1);
+    FOR(i, 1, n) {
+        int x = lower_bound(b+1, b+n+1, a[i]) - b;
+        cout << x - 1 << ' ';
+    }
+}
+    
