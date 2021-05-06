6192bd1546e5e559f1fa52c5d692709e21249a0b
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-05-06 15:00:55 +0700

    Create DPKNSNA1.cpp

---
+++ QHĐ - Cái túi (Knapsack)/DPKNSNA1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m, w, v;
+long long f[1000005];
+
+int main()
+{
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> m >> n;
+    for(int i=1; i<=n; i++) {
+        cin >> v >> w;
+        for(int j=m; j>=w; j--) f[j] = max(f[j], f[j-w] + v);
+    }
+    cout << f[m];
+}
+    
