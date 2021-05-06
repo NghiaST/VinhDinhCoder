39d3665402f5c923240cc561aa5eae4b8fd93ad2
Lê Hữu Nghĩa :      SaoST / NghiaST / nghiass001 <69393345+NghiaST@users.noreply.github.com>
2021-05-06 09:57:25 +0700

    Create BRUBIENDOI.cpp

---
+++ Duyệt - Vét cạn/BRUBIENDOI.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    scanf("%d%d", &n, &k);
+    while (k--) {
+        if (n%10 == 0) n/=10; else n--;
+    }
+    cout << n;
+}
+    
